d2eb3af4aa51c7d0449247cb15f6aa37099e6b2b
Kamal Modi <iitiankamal15@gmail.com>
2026-02-05 17:09:51 +0530

    feat: update Footer component to replace lengthy disclaimers with a concise paper trading notice

--- frontend/src/landing_page/Footer.jsx
+++ frontend/src/landing_page/Footer.jsx
@@ -191,77 +191,15 @@ const Footer = () => {
         <div className="border-t border-gray-300 pt-8">
           <div className="mb-8">
             <div className="space-y-4 text-xs text-gray-600">
-              <p>
-                EquityIQ Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI
-                Registration no.: INZ000031633 CDSL/NSDL: Depository services
-                through EquityIQ Broking Ltd. – SEBI Registration no.:
-                IN-DP-431-2019 Registered Address: EquityIQ Broking Ltd.,
-                #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public
-                School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka,
-                India. For any complaints pertaining to securities broking
-                please write to complaints@equityiq.com, for DP related to
-                dp@equityiq.com. Please ensure you carefully read the Risk
-                Disclosure Document as prescribed by SEBI | ICF
-              </p>
-              <p>
-                Procedure to file a complaint on SEBI SCORES: Register on SCORES
-                portal. Mandatory details for filing complaints on SCORES: Name,
-                PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
-                Communication, Speedy redressal of the grievances
-              </p>
-              <p>
-                Smart Online Dispute Resolution | Grievances Redressal Mechanism
-              </p>
-              <p>
-                Investments in securities market are subject to market risks;
-                read all the related documents carefully before investing.
-              </p>
-              <p>
-                Attention investors: 1) Stock brokers can accept securities as
-                margins from clients only by way of pledge in the depository
-                system w.e.f September 01, 2020. 2) Update your e-mail and phone
-                number with your stock broker / depository participant and
-                receive OTP directly from depository on your e-mail and/or
-                mobile number to create pledge. 3) Check your securities / MF /
-                bonds in the consolidated account statement issued by NSDL/CDSL
-                every month.
-              </p>
-              <p>
-                India's largest broker based on networth as per NSE. NSE broker
-                factsheet
-              </p>
-              <p>
-                "Prevent unauthorised transactions in your account. Update your
-                mobile numbers/email IDs with your stock brokers. Receive
-                information of your transactions directly from Exchange on your
-                mobile/email at the end of the day. Issued in the interest of
-                investors. KYC is one time exercise while dealing in securities
-                markets - once KYC is done through a SEBI registered
-                intermediary (broker, DP, Mutual Fund etc.), you need not
-                undergo the same process again when you approach another
-                intermediary." Dear Investor, if you are subscribing to an IPO,
-                there is no need to issue a cheque. Please write the Bank
-                account number and sign the IPO application form to authorize
-                your bank to make payment in case of allotment. In case of non
-                allotment the funds will remain in your bank account. As a
-                business we don't give stock tips, and have not authorized
-                anyone to trade on behalf of others. If you find anyone claiming
-                to be part of EquityIQ and offering such services, please create
-                a ticket here.
-              </p>
-              <p>
-                *Customers availing insurance advisory services offered by Ditto
-                (Tacterial Consulting Private Limited | IRDAI Registered
-                Corporate Agent (Composite) License No CA0738) will not have
-                access to the exchange investor grievance redressal forum, SEBI
-                SCORES/ODR, or arbitration mechanism for such products.
-              </p>
+              <center>⚠️ This is a <b>Paper Trading & Analysis Feature</b>. Trades are simulated and
+  no real money is involved. For educational purposes only.
+              </center>
             </div>
           </div>
 
           {/* Footer Links */}
-          <div>
-            <ul className="flex flex-wrap gap-4 text-xs text-gray-600">
+          <center>
+            <ul className="flex flex-wrap gap-4 text-xs text-gray-600 px-90 text-center justify-center">
               <li className="cursor-pointer">
                 <a
                   href="https://www.nseindia.com/"
@@ -311,7 +249,7 @@ const Footer = () => {
                 <Link to="/investor-charter">Investor charter</Link>
               </li>
             </ul>
-          </div>
+          </center>
         </div>
       </div>
     </footer>
